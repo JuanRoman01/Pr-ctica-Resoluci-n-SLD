b09c37afe9ef044801d291eceb8fc7cdd1226dea
JuanRoman01 <al22760245@ite.edu.mx>
2025-10-20 19:17:19 -0700

    Base de conocimientos

---
+++ base_conocimientos.py
@@ -0,0 +1,82 @@
+Hechos = {
+    "Personas": {
+        "Juan": {
+            "Municipio": "Ensenada",
+            "Identificacion": "Vigente",
+            "TipoPersona": "Fisica",
+            "Discapacidad": False,
+            "Vehiculo": {
+                "ID": "auto_juan",
+                "Tipo": "Automovil",
+                "Foraneo": False,
+                "Registrado": True
+            },
+            "Documentos": {
+                "AltaVehicular": [
+                    "ComprobanteDomicilio",
+                    "LicenciaConducirVigente",
+                    "FacturaOriginal",
+                    "PagoDerechos",
+                    "SeguroResponsabilidadCivil"
+                ],
+                "BajaVehicular": [
+                    "TarjetaCirculacion",
+                    "Placas",
+                    "PagoDerechos"
+                ]
+            }
+        },
+        "Felipe": {
+            "Municipio": "Ensenada",
+            "Identificacion": "Vigente",
+            "TipoPersona": "Fisica",
+            "Discapacidad": False,
+            "Vehiculo": {
+                "ID": "moto_felipe",
+                "Tipo": "Motocicleta",
+                "Foraneo": False,
+                "Registrado": False
+            },
+            "Documentos": {
+                "AltaVehicular": [
+                    "ComprobanteDomicilio",
+                    "LicenciaConducirVigente",
+                    "FacturaOriginal",
+                    "PagoDerechos",
+                    "SeguroResponsabilidadCivil"
+                ],
+                "BajaVehicular": [
+                    "TarjetaCirculacion",
+                    "Placas",
+                    "PagoDerechos"
+                ]
+            }
+        },
+        "EmpresaABC": {
+            "Municipio": "Ensenada",
+            "IdentificacionRepresentante": "Vigente",
+            "TipoPersona": "Moral",
+            "Vehiculo": {
+                "ID": "camion_abc",
+                "Tipo": "Camion",
+                "Foraneo": True,
+                "Registrado": True
+            },
+            "Documentos": {
+                "AltaVehicular": [
+                    "ComprobanteDomicilio",
+                    "EscrituraConstitutiva",
+                    "CedulaFiscalRFC",
+                    "PagoDerechos",
+                    "SeguroResponsabilidadCivil",
+                    "SolicitudForaneo"
+                ],
+                "BajaVehicular": [
+                    "Placas",
+                    "TarjetaCirculacion",
+                    "PagoDerechos"
+                ]
+            }
+        }
+    }
+}
